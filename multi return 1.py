f164825a18e337d17bfd76e5287a9357b65d528b
Mary Gwen Susa <Mary Gwen G. Susa>
2021-11-06 22:38:16 +0800

    Header function

---
+++ multi return 1.py
@@ -0,0 +1,12 @@
+def header():
+    print(f"Personal Identification")
+
+def obtainInfo():
+    yourName = input("Name: ")
+    yourAge = int(input("Age: "))
+    yourAddress = input("Address: ")
+    return print (f"Hi, my name is {yourName}. I am {yourAge} years old and I live in {yourAddress}.")
+    
+
+header()
+obtainInfo()
