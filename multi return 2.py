1bb42a8f6a578ec4e0bde529289d2761ad6cf349
Mary Gwen Susa <Mary Gwen G. Susa>
2021-11-08 13:37:48 +0800

    Another version prog 2

--- multi return 2.py
+++ multi return 2.py
@@ -1,4 +1,3 @@
-
 def welcome():
     print(f"Welcome to Apple and Orange Cart!")
 
@@ -16,3 +15,28 @@ def total(computeAppleQ, computeOrangeQ):
 welcome()
 numberOfApple, numberOfOrange = computeFruitQ()
 total(numberOfApple, numberOfOrange)
+
+
+# another version
+def welcome():
+    print(f"Welcome to Apple and Orange Cart!")
+
+def computeFruitQ():
+    appleQ = int(input("How many apples do you want to buy?\n> "))
+    orangeQ = int(input("How many oranges do you want to buy?\n> "))
+    return appleQ, orangeQ
+
+def fruitPrices(applePrice, orangePrice):
+    applePrice = applePrice*20
+    orangePrice = orangePrice*25
+    return applePrice, orangePrice
+
+def total(computeAppleQ, computeOrangeQ):
+    totalAmount = computeAppleQ+computeOrangeQ
+    return print(f"The total amount is {totalAmount}.")
+
+welcome()
+numberOfApple, numberOfOrange = computeFruitQ()
+fruitPrices(numberOfApple, numberOfOrange)
+totalApple, totalOrange = fruitPrices(numberOfApple, numberOfOrange)
+total(totalApple, totalOrange)
