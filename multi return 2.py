bd131924c5fe27aecdb3f66d2ebe0f8eb707ffe9
Mary Gwen Susa <Mary Gwen G. Susa>
2021-11-08 12:11:47 +0800

    Compute fruit function with fixed prices in prog 2

--- multi return 2.py
+++ multi return 2.py
@@ -1,3 +1,10 @@
 
 def welcome():
     print(f"Welcome to Apple and Orange Cart!")
+
+def computeFruitQ():
+    appleQ = int(input("How many apples do you want to buy?\n> "))
+    appleQ*=20
+    orangeQ = int(input("How many oranges do you want to buy?\n> "))
+    orangeQ*=25
+    return appleQ, orangeQ
