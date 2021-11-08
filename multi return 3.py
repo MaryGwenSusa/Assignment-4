b66f73da4481dd44af958917efb9cf2dc2229dab
Mary Gwen Susa <Mary Gwen G. Susa>
2021-11-08 15:01:24 +0800

    Wrong version from the instruction

--- multi return 3.py
+++ multi return 3.py
@@ -14,18 +14,3 @@ def computation(money, price):
 openStatement()
 money, fluctuationPrice = moneyMonitor()
 computation(money, fluctuationPrice)
-
-
-# another version
-def openStatement():
-    print(f"Budget for Comfort Food: Apple")
-
-def moneyMonitor():
-    yourWallet = float(input('Your Current Money:\n> '))
-    priceOfApple = float(input('How much is the apple?\n> '))
-    maximumNumberOfApples = int(yourWallet/priceOfApple)
-    remainingMoney = float(yourWallet%priceOfApple)
-    return yourWallet, priceOfApple, print(f"You can buy {maximumNumberOfApples} and your change is {remainingMoney:.2f}.")
-
-openStatement()
-moneyMonitor()
